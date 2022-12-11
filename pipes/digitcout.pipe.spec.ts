803e2d8ac259088b49fee1e7164f0943c8c58f6f
sureshdhok <109168867+sureshdhok@users.noreply.github.com>
2022-12-11 18:16:18 +0530

    complete all files in angular
    complete all files in angular

--- pipes/digitcout.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DigitcoutPipe } from './digitcout.pipe';
-
-describe('DigitcoutPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DigitcoutPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
